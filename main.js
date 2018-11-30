39d4dff6bcd9e7d67abdef1ba3ae22d570993c70
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-11-29 16:21:59 -0800

    added ajax send

--- main.js
+++ main.js
@@ -12,4 +12,17 @@ function initializeHandlers(){
 
 function uploadFileToServer(){
 
+	const formData = new FormData( document.querySelector('#uploadForm'));
+	$.ajax({
+		url: 'php/filereceiver.php',
+		data: formData,
+		mimeType: 'multipart/form-data',
+		contentType: false,
+		processData: false,
+		type: 'POST',
+	}).then( handleFileUploadSuccess )
+}
+
+function handleFileUploadSuccess(){
+	console.log('success!')
 }
